295f5deb7829e39d9b60ee804f4e8b31de1afd3b
Kirilas M <kirilasmichailovas@gmail.com>
2022-12-21 15:43:04 +0200

    Implement CRUD tests

--- src/test/java/myproject/FV_webApp/MovieRepositoryTest.java
+++ src/test/java/myproject/FV_webApp/MovieRepositoryTest.java
@@ -9,20 +9,62 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Rollback(false)
+@Rollback(value = true)
 public class MovieRepositoryTest {
 
     @Autowired private MovieRepository repo;
 
     @Test
-    public void testAddNew() {
+    public void testAddNewUserData() {
         Movie review = new Movie();
-        review.setUserReview("Wow nice");
+        review.setUserID(15);
+        review.setUserReview("HERE IS A REVIEW");
         review.setUserRating(5);
 
         Movie savedReview = repo.save(review);
+    }
 
+    @Test
+    public void testAddNewMovie() {
+        Movie movie = new Movie();
+        movie.setMovieTitle("The Rock");
+
+        Movie savedMovie = repo.save(movie);
+    }
+
+    @Test
+    public void testListAll() {
+        Iterable<Movie> movies = repo.findAll();
+
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        Integer userID = 1;
+        Optional<Movie> optionalMovie = repo.findById(userID);
+        Movie movie = optionalMovie.get();
+        movie.setUserReview("NEW REVIEW");
+        repo.save(movie);
+    }
+
+    @Test
+    public void testGet() {
+        Integer movieID = 3;
+        Optional<Movie> optionalMovie = repo.findByMovieID(movieID);
+        Movie movie = optionalMovie.get();
+        System.out.println(optionalMovie.get());
+    }
+
+    @Test
+    public void testDelete() {
+        Integer userID = 1;
+        repo.deleteById(userID);
     }
 }
