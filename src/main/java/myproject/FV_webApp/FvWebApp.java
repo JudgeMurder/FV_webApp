54dc72f756c2df93b1e6be0f775ee382f757d0ac
Kirilas M <kirilasmichailovas@gmail.com>
2022-12-21 15:39:22 +0200

    Change to String override

--- src/main/java/myproject/FV_webApp/FvWebApp.java
+++ src/main/java/myproject/FV_webApp/FvWebApp.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FvWebApp {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FvWebApp.class, args);
 	}
-
 }
